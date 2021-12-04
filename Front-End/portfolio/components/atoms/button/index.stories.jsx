800dc2197de9937292e2713977143ad008ef59ad
Punkinut <heypunkinut@gmail.com>
2021-12-04 17:15:54 +1100

    Created basic story for button component

---
+++ Front-End/portfolio/components/atoms/button/index.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '.';
+
+export default {
+    title: 'Atoms/Button',
+    component: Button,
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const SimpleButton = Template.bind({});
+
+SimpleButton.args = {
+    value: 'Simple Button.',
+};
